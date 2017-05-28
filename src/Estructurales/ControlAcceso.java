58d6a5fafb665e554191876bff9c32f8e60aa333
Leykat <lecvargasm@correo.udistrital.edu.co>
2017-05-27 21:38:23 -0500

    Update ControlAcceso.java

--- src/Estructurales/ControlAcceso.java
+++ src/Estructurales/ControlAcceso.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package facade;
 
-/**
- *
- * @author LEIDY K
- */
 public class ControlAcceso {
 
     private String codigoBarra = "parking2017";
