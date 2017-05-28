5f7acb7714578f32c173416e8c63ca71d07b1790
Leykat <lecvargasm@correo.udistrital.edu.co>
2017-05-27 21:39:05 -0500

    Update TiqueteraParqueadero.java

--- src/Estructurales/TiqueteraParqueadero.java
+++ src/Estructurales/TiqueteraParqueadero.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package facade;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Random;
 
-/**
- *
- * @author LEIDY K
- */
 public class TiqueteraParqueadero {
          private Calendar unaFecha;
          
