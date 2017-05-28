768d0ba3280423231b899de47f48ba495af63dc7
Leykat <lecvargasm@correo.udistrital.edu.co>
2017-05-27 21:40:58 -0500

    Update Informacion.java

--- src/Comportamiento/Informacion.java
+++ src/Comportamiento/Informacion.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package visitante;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author LEIDY K
- */
 public class Informacion implements IVisitor {
 
     public Informacion() {
