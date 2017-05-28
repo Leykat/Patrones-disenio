9a2647a98e6d0ce67e73e1d9452d2eabb501ec31
Leykat <lecvargasm@correo.udistrital.edu.co>
2017-05-27 21:44:06 -0500

    Update ProductoObserverSistemaTarifario.java

--- src/Comportamiento/ProductoObserverSistemaTarifario.java
+++ src/Comportamiento/ProductoObserverSistemaTarifario.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package observador;
 
 import java.math.BigDecimal;
 
-/**
- *
- * @author LEIDY K
- */
 public class ProductoObserverSistemaTarifario implements ObserverSistemaTarifario {
  private ProductoObservableTarifa objObservado;
     // ---------------------------------
