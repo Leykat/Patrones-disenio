856656d4b731f24bc16f259b1550b095c735123c
Leykat <lecvargasm@correo.udistrital.edu.co>
2017-05-27 21:45:47 -0500

    Update BahiaVehiculo.java

--- src/Creacionales/BahiaVehiculo.java
+++ src/Creacionales/BahiaVehiculo.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Creacionales;
 
-/**
- *
- * @author LEIDY K
- */
 public abstract  class BahiaVehiculo {
     private String id;
     private String  tipoVehiculo;
