7f3634d7613816a574c0e2726b8814d127aedacb
Leykat <lecvargasm@correo.udistrital.edu.co>
2017-05-27 21:42:43 -0500

    Update ObservableTarifa.java

--- src/Comportamiento/ObservableTarifa.java
+++ src/Comportamiento/ObservableTarifa.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package observador;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author LEIDY K
- */
 public abstract class ObservableTarifa {
     
     private ArrayList<ObserverSistemaTarifario > observadores = new ArrayList<ObserverSistemaTarifario >();
